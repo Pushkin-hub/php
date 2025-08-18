db3f38b3da8a68c6de94aec3c84ff28ec41da00d
Pushkin-hub <pushkinanton2002@gmail.com>
2025-08-18 21:26:16 +0300

    fffffff

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <p></p>
+        <p>
+            <?php echo "Hello, world!\n" ?>
+        </p>
+    </div>
+    <div>
+        <p></p>
+        <li><?php echo "Hello\nWorld!\n" ?></li>
+        <li><?php echo "Hello\n\tworld!" ?></li>
+        <li><?php echo "Hello, world. \"It's my page!\"" ?></li>
+        <p>
+            <?php echo "Hello\nWorld!\n" ?>
+        </p>
+    </div>
+        <div>
+        <p></p>
+        <p></p>
+    </div>
+</body>
+</html>
+
+
